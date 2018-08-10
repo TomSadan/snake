b8ae55d78e8bda1aa33d4d1633b80ffa40adbd40
Tom Sadan <tomsadan2@gmail.com>
2018-08-10 00:08:37 -0400

    Got dynamic refreshing working

--- src/driver/DisplayDriver.java
+++ src/driver/DisplayDriver.java
@@ -6,36 +6,40 @@ import javax.swing.JFrame;
 public class DisplayDriver extends Canvas {
 
 	/**
-	 * 
+	 * Main Driver
 	 */
 	private static final long serialVersionUID = 1L;
-	private static DisplayDriver display;
 	private static JFrame frame;
 	private static final int WIDTH = 640, HEIGHT = 480;
+	private static int x = 100;
 
 	public void paint(Graphics g) {
-		g.drawString("Hello", 40, 40);
-		setBackground(Color.WHITE);
-		g.fillRect(130, 30, 100, 80);
-		g.drawOval(30, 130, 50, 60);
-		setForeground(Color.RED);
-		g.fillOval(130, 130, 50, 60);
-		g.drawArc(30, 200, 40, 50, 90, 60);
-		g.fillArc(30, 130, 40, 50, 180, 40);
+		g.setColor(new Color(x, 55, 255));
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		/*
+		 * setBackground(Color.WHITE); g.fillRect(130, 30, 100, 80); g.drawOval(30, 130,
+		 * 50, 60); setForeground(Color.RED); g.fillOval(130, 130, 50, 60);
+		 * g.drawArc(30, 200, 40, 50, 90, 60); g.fillArc(30, 130, 40, 50, 180, 40);
+		 */
 
 	}
 
 	public void setUp() {
-
+		frame = new JFrame();
+		frame.add(this);
+		frame.setSize(WIDTH, HEIGHT);
+		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
-		display = new DisplayDriver();
-		frame = new JFrame();
-		frame.add(display);
-		frame.setSize(WIDTH, HEIGHT);
-		// f.setLayout(null);
-		frame.setVisible(true);
+		DisplayDriver driver = new DisplayDriver();
+		driver.setUp();
+		while (true) {
+			x = (x + 1) % 255;
+			System.out.println(x);
+			driver.repaint();
+			frame.revalidate();
+		}
 	}
 
 }
